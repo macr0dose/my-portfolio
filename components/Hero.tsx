d895136f98811491ceebffb98b496b6e7340429b
macr0dose <121298889+macr0dose@users.noreply.github.com>
2023-11-09 22:03:41 +1000

    first commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -19,7 +19,7 @@ const Hero = () => {
     <section className="bg-gradient-to-b from-transparent to-slate-50 max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:p-20 xl:flex-row rounded-5xl">
       <div className="relative z-20 flex flex-col xl:w-1/2">
         <h1 className="bold-52 lg:bold-88">built by</h1>
-        <h2 className="text-orange-50 bold-52 lg:bold-88">macr0dose11</h2>
+        <h2 className="text-orange-50 bold-52 lg:bold-88">macr0dose</h2>
         <p className="bold-20 mt-6 text-gray-30 xl:max-w-[520px]">
           Creating functional projects with passion. Come and discover for
           yourself.
