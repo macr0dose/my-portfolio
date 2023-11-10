186341f48f7d021e813b15d734eaf2cdfa244634
macr0dose <121298889+macr0dose@users.noreply.github.com>
2023-11-10 23:15:37 +1000

    testing contact form

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -62,27 +62,7 @@ export function ContactForm() {
                         I'll be on the other side waiting to chat!
                       </p>
                     </div>
-                    <form name="contact" method="POST" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
-                    {/* <form
+                    <form
                       name="contact"
                       method="POST"
                       data-netlify="true"
@@ -144,7 +124,7 @@ export function ContactForm() {
                           Send Message
                         </button>
                       </div>
-                    </form> */}
+                    </form>
                     {/* <div className="mt-4">
                       <button
                         type="button"
