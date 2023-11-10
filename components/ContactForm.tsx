1d396ce6294defdaf93dca31a6eceb19a4e86de0
macr0dose <121298889+macr0dose@users.noreply.github.com>
2023-11-11 00:00:35 +1000

    testing contact form for netlify

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -178,7 +178,7 @@ export function ContactForm() {
                           Send Message
                         </button>
                       </div>
-                    </form> */}
+                    </form> 
                     {/* <div className="mt-4">
                       <button
                         type="button"
