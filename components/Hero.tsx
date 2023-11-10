f7979e5784844407f24b7128850e11447f074fb1
macr0dose <121298889+macr0dose@users.noreply.github.com>
2023-11-10 23:03:54 +1000

    testing contact form

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -29,6 +29,27 @@ const Hero = () => {
           Live, Learn & Level Up.
         </p>
 
+        <form type="hidden" name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
+
         <div className="my-8 flex-col"></div>
         <div className="flex gap-5">
           <Button
