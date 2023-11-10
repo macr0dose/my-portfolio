bf85d1a25def6e09c7e483885566c41ab453fb6c
macr0dose <121298889+macr0dose@users.noreply.github.com>
2023-11-10 23:35:20 +1000

    testing contact form

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -7,6 +7,35 @@ import Button from "./Button";
 export function ContactForm() {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
+
+  const handleSubmit = async (event) => {
+    event.preventDefault(); // Prevent the default form submit
+
+    const form = new FormData(event.target);
+
+    try {
+      const response = await fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: new URLSearchParams(form).toString(),
+      });
+
+      // Handle the response from the server
+      if (response.ok) {
+        // Form submission successful, do something like redirect to a thank-you page
+        console.log('Form submitted successfully!');
+      } else {
+        // Handle errors
+        console.error('Form submission failed!');
+      }
+    } catch (error) {
+      console.error('An error occurred:', error);
+    }
+  };
+
+
+
+
   const closeModal = () => {
     setIsModalOpen(false);
   };
@@ -62,7 +91,33 @@ export function ContactForm() {
                         I'll be on the other side waiting to chat!
                       </p>
                     </div>
-                    <form
+
+
+                    <form name="contact" method="POST" data-netlify="true" onSubmit={handleSubmit}>
+      <input type="hidden" name="form-name" value="contact" />
+      <p>
+        <label htmlFor="name">Your Name:</label>
+        <input type="text" id="name" name="name" required />
+      </p>
+      <p>
+        <label htmlFor="email">Your Email:</label>
+        <input type="email" id="email" name="email" required />
+      </p>
+      <p>
+        <label htmlFor="message">Message:</label>
+        <textarea id="message" name="message" required></textarea>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+
+
+
+
+
+                    
+                    {/* <form
                       name="contact"
                       method="POST"
                       data-netlify="true"
@@ -124,7 +179,7 @@ export function ContactForm() {
                           Send Message
                         </button>
                       </div>
-                    </form>
+                    </form> */}
                     {/* <div className="mt-4">
                       <button
                         type="button"
