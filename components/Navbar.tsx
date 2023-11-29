1ba756d6035d189a8bcdbc363679f0839f436121
macr0dose <121298889+macr0dose@users.noreply.github.com>
2023-11-29 18:43:21 +1000

    add dark mode

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 import { Transition } from "@headlessui/react";
 import DarkModeToggle from "./DarkModeToggle";
 
-import { useTheme } from 'next-themes';
+import { useTheme } from "next-themes";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -23,16 +23,18 @@ const Navbar = () => {
     }
   };
 
-  const handleClickOutside = (event: MouseEvent) => {
-    if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+  const handleClickOutside = (event: Event) => {
+    const target = event.target as Node;
+
+    if (menuRef.current && !menuRef.current.contains(target)) {
       setIsMenuOpen(false);
     }
   };
 
   useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside as EventListener);
+    document.addEventListener("mousedown", handleClickOutside);
     return () => {
-      document.removeEventListener("mousedown", handleClickOutside as EventListener);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
 
@@ -66,52 +68,55 @@ const Navbar = () => {
 
       <DarkModeToggle />
       <div ref={menuRef}>
-      <button onClick={() => setIsMenuOpen(!isMenuOpen)} className="lg:hidden">
-        {theme === 'dark' ? (
-          <Image
-            src="/menu-dark.webp" // Path to white hamburger icon for dark mode
-            alt="menu"
-            width={32}
-            height={32}
-            className="cursor-pointer"
-          />
-        ) : (
-          <Image
-            src="/menu.webp" // Path to default hamburger icon for light mode
-            alt="menu"
-            width={32}
-            height={32}
-            className="cursor-pointer"
-          />
-        )}
-      </button>
+        <button
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          className="lg:hidden"
+        >
+          {theme === "dark" ? (
+            <Image
+              src="/menu-dark.webp" // Path to white hamburger icon for dark mode
+              alt="menu"
+              width={32}
+              height={32}
+              className="cursor-pointer"
+            />
+          ) : (
+            <Image
+              src="/menu.webp" // Path to default hamburger icon for light mode
+              alt="menu"
+              width={32}
+              height={32}
+              className="cursor-pointer"
+            />
+          )}
+        </button>
 
-      <Transition
-        show={isMenuOpen}
-        enter="transition-opacity ease-linear duration-300"
-        enterFrom="opacity-0"
-        enterTo="opacity-100"
-        leave="transition-opacity ease-linear duration-300"
-        leaveFrom="opacity-100"
-        leaveTo="opacity-0"
-        className="absolute top-full right-0 mt-2 bg-slate-200 rounded-xl lg:hidden shadow-lg"
-      >
-        <ul>
-          {NAV_LINKS.map((link) => (
-            <li
-              key={link.key}
-              className="p-2 px-8 rounded-xl regular-24 text-black"
-            >
-              <Link
-                href={link.href}
-                onClick={(e) => smoothScroll(e, link.href)}
+        <Transition
+          show={isMenuOpen}
+          enter="transition-opacity ease-linear duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="transition-opacity ease-linear duration-300"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+          className="absolute top-full right-0 mt-2 bg-slate-200 rounded-xl lg:hidden shadow-lg"
+        >
+          <ul>
+            {NAV_LINKS.map((link) => (
+              <li
+                key={link.key}
+                className="p-2 px-8 rounded-xl regular-24 text-black"
               >
-                {link.label}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </Transition>
+                <Link
+                  href={link.href}
+                  onClick={(e) => smoothScroll(e, link.href)}
+                >
+                  {link.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </Transition>
       </div>
     </nav>
   );
