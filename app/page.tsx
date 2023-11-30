ec2e476a7a2dd2d913474c25dc2939cb9988170b
macr0dose <121298889+macr0dose@users.noreply.github.com>
2023-11-30 15:22:45 +1000

    change font from css import to fonts folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Head from 'next/head';
+
 import Hero from "@/components/Hero";
 import Scroller from "@/components/Scroller";
 import Projects from "@/components/Projects";
@@ -5,9 +7,11 @@ import Skillset from "@/components/Skillset";
 import About from "@/components/About";
 import Contact from "@/components/Contact";
 
+
 export default function Home() {
   return (
     <>
+
       <Hero />
       <Scroller />
       <Skillset />
