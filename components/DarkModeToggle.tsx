497c79141903f4633b4f6d20b3675ab41404bbbb
macr0dose <121298889+macr0dose@users.noreply.github.com>
2023-12-01 18:08:02 +1000

    update navbar dark toggle

--- components/DarkModeToggle.tsx
+++ components/DarkModeToggle.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { useTheme } from 'next-themes';
-import { FiSun, FiMoon } from 'react-icons/fi'; // Import icons
+import React, { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
+import { FiSun, FiMoon } from "react-icons/fi"; // Import icons
 
 const DarkModeToggle = () => {
   const { theme, setTheme, systemTheme } = useTheme();
@@ -12,24 +12,25 @@ const DarkModeToggle = () => {
 
   if (!mounted) return null;
 
-  const currentTheme = theme === 'system' ? systemTheme : theme;
-  const isDark = currentTheme === 'dark';
+  const currentTheme = theme === "system" ? systemTheme : theme;
+  const isDark = currentTheme === "dark";
 
   const handleToggle = () => {
-    setTheme(isDark ? 'light' : 'dark');
+    setTheme(isDark ? "light" : "dark");
   };
 
   return (
     <label className="toggle-switch">
       <input
+        id="darktoggle"
         type="checkbox"
         checked={isDark}
         onChange={handleToggle}
         aria-label="Toggle Dark/Light Mode"
       />
       <span className="slider round">
-        <FiSun className={`icon sun ${isDark ? 'hidden' : ''}z-10`} />
-        <FiMoon className={`icon moon ${isDark ? '' : 'hidden'}z-10`} />
+        <FiSun className={`icon sun ${isDark ? "hidden" : ""}z-10`} />
+        <FiMoon className={`icon moon ${isDark ? "" : "hidden"}z-10`} />
       </span>
     </label>
   );
